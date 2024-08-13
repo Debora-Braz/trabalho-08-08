c8dc13da05ec2af66bab737c4fdd5ef0125114a2
Débora da Silva Braz Pereira <deboradasbrazp2006@gmail.com>
2024-08-13 11:38:56 -0300

    trabalho

---
+++ trabalho.php
@@ -0,0 +1,24 @@
+<?php
+    include "conecta.php";
+?>
+
+
+<?php
+    echo "<b>Mostrando todos os registros</b><br>";
+    $sql = "SELECT id, descricao, fabricante, quantidade, preco_custo, preco_venda FROM produtos";
+    $resultado = mysqli_query($conexao, $sql);
+    echo "<br>";
+  
+    if (mysqli_num_rows($resultado) > 0){
+        while($registro = mysqli_fetch_assoc($resultado)) {
+        echo "id: " . $registro["id"]. " - Descrição: " . $registro["descricao"].
+        " - Fabricante:" . $registro["fabricante"]. " - Quantidade:" . $registro["quantidade"].
+        " - Preço de custo:" . $registro["preco_custo"]. " - Preço de venda:" . $registro["preco_venda"].
+        "<br>";
+        }
+    } else{
+        echo "Nenhum registro encontrado.";
+    }
+
+    
+?>
